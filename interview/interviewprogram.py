95b8f6acf2d722de414f0e12f103990cfcacb0ef
darkphysicist <34658531+darkphysicist@users.noreply.github.com>
2022-05-05 14:45:21 -0600

    Update interviewprogram.py

--- interview/interviewprogram.py
+++ interview/interviewprogram.py
@@ -1,16 +1,18 @@
 
-pip install pandas
 import sys
+import csv
 import pandas as pd
 import importlib
 import argparse
 parser = argparse
 var = sys.argv[1]
-importlib.import_module
+importlib.import_module(var)
+var.to_csv('schedule.csv')
+var.index
+var = read_csv('schedule.csv', index_col=0)
+var.describe(include = object)
+#this line is for lookups based on schedule.csv
+app = sys.argv[2]
+importlib.import_module(app)
 #this code is for the csv file
 def from_dict(cls, column: dict[str, str]) - 
-> "Schedule":
-    if column["Disabled"] = true 
-    return None
-    else
-    return row
