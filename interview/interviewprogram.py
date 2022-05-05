8a80632380a11caf690dabe820388b3c778b2620
darkphysicist <34658531+darkphysicist@users.noreply.github.com>
2022-05-05 11:36:39 -0600

    Update interviewprogram.py

--- interview/interviewprogram.py
+++ interview/interviewprogram.py
@@ -1,14 +1,12 @@
-// This will be the connector to the files
-# based on what is put into the command line I want to create 
-a path to the schedule file, the chosen app file, and the 
-#output example file where the result will go
-#the code will create a path to the appfile chosen and look at the names
-#each name will be compared to the same name in the schedule text file
-#Once the correct one is found it will then print out everything within that row
 
+pip install pandas
+import sys
+import pandas as pd
+import importlib
 import argparse
 parser = argparse
-
+var = sys.argv[1]
+importlib.import_module
 #this code is for the csv file
 def from_dict(cls, column: dict[str, str]) - 
 > "Schedule":
