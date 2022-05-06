17dcf433d524979010cd8f73e5148b018c622e63
darkphysicist <34658531+darkphysicist@users.noreply.github.com>
2022-05-05 21:30:01 -0600

    Update interviewprogram.py

--- interview/interviewprogram.py
+++ interview/interviewprogram.py
@@ -8,11 +8,21 @@ parser = argparse
 var = sys.argv[1]
 importlib.import_module(var)
 var.to_csv('schedule.csv')
-var.index
-var = read_csv('schedule.csv', index_col=0)
+colspecs = [(2, 20), (22, 62), (64, 72), (77, 85), (88, 93), (99, 107)}
+var.pd.read_fwf('schedule.txt', skiprows = 2, colspecs = colspecs, names = ['Application', 'Options', 'Start', 'Disabled', 'Schedule'])
 var.describe(include = object)
 #this line is for lookups based on schedule.csv
 app = sys.argv[2]
 importlib.import_module(app)
-#this code is for the csv file
-def from_dict(cls, column: dict[str, str]) - 
+colspecs = [(2, 20), (22, 62), (64, 72), (77, 85), (88, 93), (99, 107)}
+filepath = app
+with open(filepath) as fp:
+            line = fp.readline()
+            cnt = 1
+            while line, i < size, i++:
+            if(fp[i] = var[i])
+            display(var.ilo[i](
+            else if(fp[i] != var[i])
+            i++
+            else
+            return 0
